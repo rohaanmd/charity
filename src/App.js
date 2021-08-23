5013d6ca044bb210b4a1ee410a52e1b7bf06371f
Mohd Rohaan <panzerxhack@gmail.com>
2021-08-23 11:48:44 +0530

    assig,mnet uploaded

--- src/App.js
+++ src/App.js
@@ -6,46 +6,45 @@ import Banner1 from "./components/banners/banner1";
 import DonateInfo from "./components/donate-info/index";
 import MiscHd1 from "./components/misc_component/header1";
 import Card1 from "./components/card/card1";
-import InfoBanner from "./components/misc_component/listing_react"
-import Card2 from "./components/card/card2"
+import InfoBanner from "./components/misc_component/listing_react";
+import Card2 from "./components/card/card2";
 import Form from "./components/contactus/index";
 import Teamslider from "./components/misc_component/teamslider";
 import StateLearn from "./components/learnState/stateLearn";
-import FormTest from "./components/form_in_depth/form"
+import FormTest from "./components/form_in_depth/form";
 import AxiosFile from "./components/INFORMATION ABOUT CONCEPT/AxiosFile";
 function App() {
 	return (
 		<div className="App">
-			{/* <Header className="kuch" />
+			{/* assignment 5 init */}
+
+			<Header className="kuch" />
 			<Navbar />
 			<Hero />
 			<Banner1 />
 			<DonateInfo />
 			<MiscHd1 />
 			<Card1 />
-      <InfoBanner />
-	  <Card2 />
-	  <Form />
-	  <Teamslider /> */}
-{/* <StateLearn /> */}
+			<InfoBanner />
+			<Card2 />
+			<Form />
+			<Teamslider />
 
-{/* <FormTest /> */}
+			{/* <StateLearn /> */}
 
-<AxiosFile />
+			{/* <FormTest /> */}
 
+			{/* <AxiosFile /> */}
 
-	  
-
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-	  
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
 		</div>
 	);
 }
